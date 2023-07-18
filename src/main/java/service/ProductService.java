2d5facb16fa1c7aed4813874a93b7dd79d900c82
marcin <marcincaban1987@gmail.com>
2023-07-18 18:13:38 +0100

    services task

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,14 +1,46 @@
 package service;
+
 import java.util.ArrayList;
 
 public class ProductService {
-    public class ProjectService {
-        private List<Product> products;
+    private List<Product> products;
 
-        public ProjectService() {
-            products = new ArrayList<>();
+    public void ProductService() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(double price, String name, Category category, int quantity) {
+        Product product = new Product(price, name, category, quantity);
+        products.add(product);
+    }
+
+    public void removeProduct(int productId) {
+        Product productToRemove = null;
+        for (Product product : products) {
+            if (product.getProductId() == productId) {
+                productToRemove = product;
+                break;
+            }
         }
-        public void addProduct(double price, String name, Category category, int quantity) {
-            Product product = new Product(price, name, category, quantity);
-            products.add(product);
+            if (productToRemove != null) {
+            products.remove(productToRemove);
+        }
+            else {
+                throw new IllegalArgumentException("Product with ID " + productId + " not found.");
+        }
+    }
+
+    public List<Product> getAllProducts() {
+        return products;
+    }
+
+    public Product getProductById(int productId) {
+        for (Product product : products) {
+            if (product.getProductId() == productId) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
 
