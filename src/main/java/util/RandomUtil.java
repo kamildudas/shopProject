c1753d1f9d9dcc9ba983308911a36b62fd340356
Kamil Dudas <kamildudas@hotmail.com>
2023-07-18 20:16:09 +0200

    Add OrderService

--- src/main/java/util/RandomUtil.java
+++ src/main/java/util/RandomUtil.java
@@ -4,9 +4,20 @@ import java.util.Random;
 
 public class RandomUtil {
     private Random random = new Random();
-    public static String randomizeString (final int length){
+    private static final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+    public static String randomizeString(final int length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("Length must be a positive");
+        }
 
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(characters.charAt(random.nextInt(characters.length())));
+        }
+
+        return sb.toString();
     }
 
 }
